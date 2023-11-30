b3a862efcc5fbe3670dfaee031a2ef36de55e93e
Gözdenur Demir <gozdemrg@gmail.com>
2023-11-30 13:58:31 +0300

    RoboHero.py

--- RoboHero.py
+++ RoboHero.py
@@ -1,4 +1,7 @@
-# Complete your game here
+"""
+University of Helsinki Advanced Course in Programming (Python MOOC 2023)
+Final project, by Gözdenur Demir, 16 Oct 2023
+"""
 import pygame,random
 
 class Sprite:
